cb53518a4b95f5ec3cd57ab0a322689cb17abde6
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-04-17 00:31:51 +0200

    added contacts view

---
+++ WTECH/resources/views/contacts.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5">
+                    <div class="contain mt-5">
+                        <h2 class="text-new mb-4">Contacts</h2>
+                        <div class="panel panel-default mx-auto">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <span class="text-contacts col-auto mx-auto"> CUSTOMER SERVICE</span>
+                                </div>
+                                <div class="row my-5">  
+                                    <span class="text-info text-white col-auto mx-auto"> +421 000 000 000</span>
+                                    <span class="text-info text-white col-auto mx-auto"> jozkomrkvicka@fiit.sk</span>
+                                </div>
+                                <div class="row">
+                                    <span class="text-date col-auto mx-auto">PO-PI</span>
+                                </div>
+                                <div class="row">
+                                    <span class="text-date col-auto mx-auto pb-4">8:00 - 16:00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contain mt-5">
+                        <div class="panel panel-default mx-auto">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <span class="text-contacts col-auto mx-auto"> COMPLAINTS</span>
+                                </div>
+                                    <div class="row my-5">
+                                        <span class="text-info text-white col-auto mx-auto"> +421 000 000 000</span>
+                                        <span class="text-info text-white col-auto mx-auto"> jozkomrkvicka@fiit.sk</span>
+                                    </div>
+                                <div class="row">
+                                    <span class="text-date col-auto mx-auto"> PO-PI</span>
+                                </div>
+                                <div class="row">
+                                    <span class="text-date col-auto mx-auto pb-4"> 8:00 - 16:00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+                
+@endsection
