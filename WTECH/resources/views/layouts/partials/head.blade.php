f95e6d96b26b38bec1331076a6af6d4746749d06
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-04-17 00:00:54 +0200

    added head partial

---
+++ WTECH/resources/views/layouts/partials/head.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>E-shop</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- link to an bootstrap library -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- link to an icon library -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.7.2/font/bootstrap-icons.min.css" rel="stylesheet">
+
+<!-- Inter font -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+
+<!-- scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> 
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+<!-- links for css -->
+<link rel="stylesheet" href="../../../css/navbar.css">
+<link rel="stylesheet" href="../../../css/base.css">
+<link rel="stylesheet" href="../../../css/footer.css">
+<link rel="stylesheet" href="../../../css/subnavbar.css">
+<link rel="stylesheet" href="../../../css/homepage.css">
