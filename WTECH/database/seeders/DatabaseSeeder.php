3671e8cb1b6925caeed672b218658046b54c48ff
Patrik Hitka <82773579+star1master@users.noreply.github.com>
2023-04-27 13:23:44 +0200

    added calls to seeders

--- WTECH/database/seeders/DatabaseSeeder.php
+++ WTECH/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(ProductSeeder::class);
+        $this->call(TagsSeeder::class);
+        $this->call(ProductsTagsSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
