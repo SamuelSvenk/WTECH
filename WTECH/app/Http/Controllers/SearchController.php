fde068a2d0368908ed41fef04f7ccc0053616e3b
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-04-27 15:45:19 +0200

    added search controller

---
+++ WTECH/app/Http/Controllers/SearchController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\console;
+use Illuminate\Http\Request;
+use DB;
+use App\Http\requests;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $query = $request->input('search');
+
+        $products = Product::where('title', 'like', "%$query%");
+
+        $products = $products->paginate(12); 
+        return view('products', ['products' => $products]);
+    }
+}
