97dd7bc038bb25346e81578747239bfcbd553d85
Patrik Hitka <82773579+star1master@users.noreply.github.com>
2023-04-29 15:32:29 +0200

    Add files via upload

--- WTECH/resources/views/products.blade.php
+++ WTECH/resources/views/products.blade.php
@@ -86,22 +86,53 @@
         </div>
         <div class="col-lg-9 col-md-9 col-sm-8 col-12"> 
             <div class="row justify-content-start justify-content-lg-between justify-content-md-between">
+                <form>
                 <div class="dropdown d-block d-sm-none">
                     <button class="btn nav-filter btn-secondary dropdown-toggle" type="button" id="filterButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Filter
                     </button>
                     <div class="dropdown-menu drop-filter" aria-labelledby="filterButton">
+                        <h6 class="dropdown-header">Price</h6>
+                        <div class="form-row">
+                            <div class="form-group">
+                                <input type="number" name="price_min" class="form-control" placeholder="$0">
+                            </div>
+                            <div class="form-group">
+                                <input type="number" name="price_max" class="form-control" placeholder="$1,000">
+                            </div>
+                        </div>
                         <h6 class="dropdown-header">Devs</h6>
-                        <a class="dropdown-item drop-filter-item" href="#">Activision</a>
-                        <a class="dropdown-item drop-filter-item" href="#">Blizzard</a>
-                        <a class="dropdown-item drop-filter-item" href="#">Nintendo</a>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="activision" value="activision" id="activisionCheck">
+                            <label class="form-check-label text-label" for="activisionCheck">Activision</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="blizzard" value="blizzard" id="blizzardCheck">
+                            <label class="form-check-label text-label" for="blizzardCheck">Blizzard</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="nintendo" value="nintendo" id="nintendoCheck">
+                            <label class="form-check-label text-label" for="nintendoCheck">Nintendo</label>
+                        </div>
                         <div class="dropdown-divider drop-filter"></div>
                         <h6 class="dropdown-header">Genres</h6>
-                        <a class="dropdown-item drop-filter-item" href="#">RPG</a>
-                        <a class="dropdown-item drop-filter-item" href="#">MOBA</a>
-                        <a class="dropdown-item drop-filter-item" href="#">FPS</a>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="rpg" value="rpg" id="rpgCheck">
+                            <label class="form-check-label text-label" for="rpgCheck">RPG</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="moba" value="moba" id="mobaCheck">
+                            <label class="form-check-label text-label" for="mobaCheck">MOBA</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input drop-filter-item" type="checkbox" name="fps" value="fps" id="fpsCheck">
+                            <label class="form-check-label text-label" for="fpsCheck">FPS</label>
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary submit-btn p-2 text-dark border-dark">SUBMIT</button>
                     </div>
                 </div>
+                </form>
                 <div class="col-4 col-md-4">
                     <nav class="navbar nav-sort navbar-expand-lg">
                         <div class="container-fluid justify-content-around">
@@ -109,14 +140,17 @@
                             <button class="btn sort-btn dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                                 Sort By
                             </button>
-                            <ul class="dropdown-menu drop-menu" aria-labelledby="dropdownMenuButton">
-                                <li><a class="dropdown-item drop-item" href="#">Price: Low to High</a></li>
-                                <li><a class="dropdown-item drop-item" href="#">Price: High to Low</a></li>
-                                <li><a class="dropdown-item drop-item" href="#">Size: Small to Large</a></li>
-                                <li><a class="dropdown-item drop-item" href="#">Size: Large to Small</a></li>
-                                <li><a class="dropdown-item drop-item" href="#">Age: New to Old</a></li>
-                                <li><a class="dropdown-item drop-item" href="#">Age: Old to New</a></li>
-                            </ul>
+                            <form action="{{route('filter')}}" method="get">
+                            @csrf
+                                <div class="dropdown-menu drop-menu" aria-labelledby="dropdownMenuButton">
+                                    <button type="submit" name="price_asc" value="price_asc" class="dropdown-item drop-item">Price: Low to High</button>
+                                    <button type="submit" name="price_desc" value="price_desc" class="dropdown-item drop-item">Price: High to Low</button>
+                                    <button type="submit" name="size_asc" value="size_asc" class="dropdown-item drop-item">Size: Small to Large</button>
+                                    <button type="submit" name="size_desc" value="size_desc" class="dropdown-item drop-item">Size: Large to Small</button>
+                                    <button type="submit" name="age_asc" value="age_asc" class="dropdown-item drop-item">Age: New to Old</button>
+                                    <button type="submit" name="age_desc" value="age_desc" class="dropdown-item drop-item">Age: Old to New</button>
+                                </div>
+                            </form>
                             </div>
                             <span class="navbar-text text-light" id="sortOption">Selected option</span>
                         </div>
@@ -138,13 +172,17 @@
             </div>
             <div class="row products justify-content-center mt-3">
             @foreach ($products as $product)
-                <div class="col-md-5 col-lg-4 col-xl-4 col-sm-6 col-6">
+                <div class="custom-col">
                     <a href="/productpage/{{ ($product->title) }}" class="card-link">
                         <div class="card card-product">
                             <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->title }}">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $product->title }}</h5>
+                                @if ($product->quantity > 0)
                                 <span class="text-av">Available</span>
+                                @else
+                                <span class="text-nav">Unavailable</span>
+                                @endif
                                 <span class="card-text">{{ $product->price }}</span>
                             </div>
                         </div>
