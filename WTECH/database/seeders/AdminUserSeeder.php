192f67c7cc09eb743b9d2a4f820e810e8a8281b5
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-04-29 17:44:22 +0200

    admin seeder

---
+++ WTECH/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminUser = [
+            'name' => 'Admin',
+            'surname' => 'User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin'
+        ];
+
+        User::create($adminUser);
+    }
+}
