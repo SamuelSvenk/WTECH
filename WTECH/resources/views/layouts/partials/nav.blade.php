ad4ade86a33cb1d1f15e00d9614229eac8b8a155
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-04-29 17:36:08 +0200

    links updated, admin icon added

--- WTECH/resources/views/layouts/partials/nav.blade.php
+++ WTECH/resources/views/layouts/partials/nav.blade.php
@@ -2,9 +2,17 @@
     <div class="container-fluid">
         <a class="navbar-brand logo-custom" href="{{ route('home') }}">LOGO</a>
         <form class="d-flex" action="{{ route('search') }}" method="GET">
-            <input class="form-control me-2 border-dark" type="search" placeholder="Search" name="search" aria-label="Search">
+            <input class="form-control me-2 border-dark" type="search" placeholder="Search" name="search"
+                aria-label="Search">
         </form>
         <div class="d-flex">
+            @auth
+            @if (Auth::user()->isAdmin())
+            <a href="{{ route('adminproducts') }}" class="d-block text-black me-3 fs-3" aria-expanded="false">
+                <i class="bi bi-clipboard"></i>
+            </a>
+            @endif
+            @endauth
             @if (Auth::check())
             <div class="dropdown ">
                 <a href="#" class="d-block text-black me-3 fs-3" id="dropdownMenuButton" data-bs-toggle="dropdown"
@@ -12,14 +20,16 @@
                     <i class="bi bi-person"></i>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-custom shadow" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item text-white dropdown-item-custom" href="{{ route('profile') }}">Profile</a></li>
-                    <li><a class="dropdown-item dropdown-item-custom" id="logout-btn" href="{{ route('logout') }}">Log Out</a></li>
+                    <li><a class="dropdown-item text-white dropdown-item-custom"
+                            href="{{ route('profile') }}">Profile</a></li>
+                    <li><a class="dropdown-item dropdown-item-custom" id="logout-btn" href="{{ route('logout') }}">Log
+                            Out</a></li>
                 </ul>
             </div>
             @else
             <a href="{{ route('register') }}" class="me-3 fs-3 text-black"><i class="bi bi-person"></i></a>
             @endif
-            <a href=".#" class="me-3 fs-3 text-black"><i class="bi bi-cart"></i></a>
+            <a href="{{ route ('cart') }}" class="me-3 fs-3 text-black"><i class="bi bi-cart"></i></a>
         </div>
     </div>
 </nav>
