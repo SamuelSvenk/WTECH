b42c60adc96ecf532c791af46173bb93759fc825
Patrik Hitka <82773579+star1master@users.noreply.github.com>
2023-05-03 01:45:43 +0200

    added links to products

--- WTECH/resources/views/layouts/partials/subnav.blade.php
+++ WTECH/resources/views/layouts/partials/subnav.blade.php
@@ -3,19 +3,19 @@
       <div class=" navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item d-block d-sm-none">
-            <a class="nav-link text-white" href="#">CATALOGUE</a>
+            <a class="nav-link text-white" href="/products">CATALOGUE</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">PLAYSTATION</a>
+            <a class="nav-link text-white" href="/products/playstation">PLAYSTATION</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">NINTENDO</a>
+            <a class="nav-link text-white" href="/products/nintendo">NINTENDO</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">PC</a>
+            <a class="nav-link text-white" href="/products/pc">PC</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">XBOX</a>
+            <a class="nav-link text-white" href="/products/xbox">XBOX</a>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
