b7f0f2f8c78066a43d502e61193360155245b431
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-05-04 18:13:49 +0200

    Update adminproducts.blade.php

--- WTECH/resources/views/adminproducts.blade.php
+++ WTECH/resources/views/adminproducts.blade.php
@@ -5,7 +5,7 @@
 <div class="container mt-5">
     <div class="row">
         <div class="col">
-            <form class="d-flex" action="{{ route('admin-filter') }}" method="GET">
+            <form class="d-flex" action="{{ route('adminproducts-filter') }}" method="GET">
                 <input class="form-control border-dark" type="search" placeholder="Search" name="search" aria-label="Search">
             </form>
         </div>
