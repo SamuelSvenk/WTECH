bfa36dadf069a8b901aa3d552dbae3dec3fb1d78
Maria Fedosenya <86833007+totes8@users.noreply.github.com>
2023-05-04 15:08:29 +0200

    fix

--- WTECH/resources/views/layouts/partials/subnav.blade.php
+++ WTECH/resources/views/layouts/partials/subnav.blade.php
@@ -6,16 +6,16 @@
             <a class="nav-link text-white" href="{{ route('products') }}">CATALOGUE</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">PLAYSTATION</a>
+            <a class="nav-link text-white" href="/products/playstation">PLAYSTATION</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">NINTENDO</a>
+            <a class="nav-link text-white" href="/products/nintendo">NINTENDO</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">PC</a>
+            <a class="nav-link text-white" href="/products/pc">PC</a>
           </li>
           <li class="nav-item d-none d-sm-block">
-            <a class="nav-link text-white" href="#">XBOX</a>
+            <a class="nav-link text-white" href="/products/xbox">XBOX</a>
           </li>
         </ul>
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
