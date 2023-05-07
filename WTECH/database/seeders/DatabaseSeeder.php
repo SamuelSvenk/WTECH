728d2a0d920fc826539447145c7993ce3c57392c
Patrik Hitka <82773579+star1master@users.noreply.github.com>
2023-05-07 19:48:21 +0200

    added admin seeder

--- WTECH/database/seeders/DatabaseSeeder.php
+++ WTECH/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductsTagsSeeder::class);
         $this->call(ImagesTableSeeder::class);
         $this->call(ProductImageSeeder::class);
+        $this->call(AdminUserSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
