36b9c10998148fedb1fd4352fa05deee23c66c4f
Patrik Hitka <82773579+star1master@users.noreply.github.com>
2023-05-07 19:01:38 +0200

    added image migrations

---
+++ WTECH/database/migrations/2023_05_06_114125_create_productimage_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('image_product', function (Blueprint $table) {
+            $table->id();
+            $table->UnsignedBigInteger("product_id");
+            $table->UnsignedBigInteger("image_id");
+            $table->timestamps();
+
+            $table->foreign("product_id")->references("id")->on("products")->onDelete("cascade");
+            $table->foreign("image_id")->references("id")->on("images")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('image_product');
+    }
+};
